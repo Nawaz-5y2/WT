e60dc0471f6a66d59dd771d1b2272027280c5bd5
Nawaz-5y2 <23311a05y2@cse.sreenidhi.ed.in>
2026-02-05 15:35:26 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function detectDevice() {
+    var deviceTypeElement = document.getElementById('deviceType');
+    var featureList = document.getElementById('featureList');
+
+    if (window.innerWidth < 768) {
+        deviceTypeElement.textContent = "You are using a mobile device.";
+        
+        featureList.innerHTML = `
+            <div class="feature">Mobile Feature 1</div>
+            <div class="feature">Mobile Feature 2</div>
+        `;
+    } else if (window.innerWidth < 1024) {
+        deviceTypeElement.textContent = "You are using a tablet.";
+       
+        featureList.innerHTML = `
+            <div class="feature">Tablet Feature 1</div>
+            <div class="feature">Tablet Feature 2</div>
+            <div class="feature">Tablet Feature 3</div>
+        `;
+    } else {
+        deviceTypeElement.textContent = "You are using a desktop.";
+        
+        featureList.innerHTML = `
+            <div class="feature">Desktop Feature 1</div>
+            <div class="feature">Desktop Feature 2</div>
+            <div class="feature">Desktop Feature 3</div>
+            <div class="feature">Desktop Feature 4</div>
+        `;
+    }
+}
+window.onload = detectDevice;
+window.onresize = detectDevice;
